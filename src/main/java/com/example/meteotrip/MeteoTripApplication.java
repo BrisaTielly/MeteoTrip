af9b0164e26500aabf64336b4fdb47c3434594e6
Brisa Silva <brisatielly12@gmail.com>
2025-03-07 09:38:17 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/meteotrip/MeteoTripApplication.java
@@ -0,0 +1,13 @@
+package com.example.meteotrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteoTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meteotrip/MeteoTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meteotrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
